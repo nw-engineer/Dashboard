70e38e894b681e12fb98575e7ffb9d79948718fd
nw-engineer <rst_pi_sisk10.vi@krf.biglobe.ne.jp>
2026-02-19 13:10:15 +0900

    Delete fastapi/return_generated.py

--- fastapi/return_generated.py
+++
@@ -1,9 +0,0 @@
-# Promptfoo Python provider
-# https://www.promptfoo.dev/docs/providers/python/  (概念: call_api を実装して output を返す)
-
-def call_api(prompt, options, context):
-    # context["vars"] に tests[].vars が入る
-    vars_ = context.get("vars", {})
-    return {
-        "output": vars_.get("generated", "")
-    }
